f0b07ed029a173e9133b440a9599765c47a6d04d
Giannis Tzonis <tzonis.john@hotmail.com>
2022-10-15 04:22:50 +0300

    created navbar and styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,11 @@
 import React from "react"
  
 export default function Navbar() {
-    return <h1>navbar components</h1>
+    return (
+        <nav>
+            <img src="../images/logo.jpg" className="nav--icon"/>
+            <h3 className="nav--logo_text">React Facts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+        )
 }
