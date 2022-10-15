8ecec18e41062ad3b6ec134059e1c95e51384ccf
Giannis Tzonis <tzonis.john@hotmail.com>
2022-10-15 04:44:07 +0300

    created main section and added styles

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,17 @@
 import React from "react"
  
 export default function Main() {
-    return <h1>Main components</h1>
+    return (
+        <main>
+            <h1 className="main--title"> Blahblah blahblah</h1>
+            <ul className="main--facts">
+                <li>Text text text text</li>
+                <li>Text text text text</li>
+                <li>Text text text text</li>
+                <li>Text text text text</li>
+                <li>Text text text text</li>
+            </ul>
+        </main>
+            
+        )
 }
