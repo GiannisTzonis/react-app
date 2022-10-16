a8b5265963632a2d0f515a5efdd01232b0b041fe
Giannis Tzonis <tzonis.john@hotmail.com>
2022-10-16 04:52:59 +0300

    added basic styles on contact

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react"
 import Navbar from "./components/Navbar"
 import Main from "./components/Main"
-import Contact from "./components/contacts"
+import Contact from "./components/Contacts"
 
 export default function App() {
     return (
         <div className="container">
             <Navbar />
             <Main />
+            <Contact />
             <Contact />
         </div>
     )
